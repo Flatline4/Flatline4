6329886965f1df4caf45592932a738a5ddff312d
Christian Cygnus <cygnus@Christians-MacBook-Pro.local>
2017-02-25 14:12:00 -0600

    Added many starter files for DOM elements

---
+++ src/Generation/DomElement.ts
@@ -0,0 +1,7 @@
+import { DomElementParent } from './DomElementParent';
+
+export abstract class DomElement {
+    private Name: string;
+    private Id: string;
+    private Parent: DomElementParent;
+}
