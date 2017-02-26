814cb44f70239a816643e178d322f2bfc2baa608
Brian Kurek <brkurek@gmail.com>
2017-02-25 21:59:09 -0600

    Update build to create normal and minified builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+const minify = JSON.parse(process.env.MINIFY);
 
 module.exports = [{
     entry: path.join(__dirname, 'src', 'Flatline.ts'),
     output: {
         path: path.join(__dirname, 'build'),
-        filename: "flatline-browser.min.js",
-        library: "Flatline",
-        libraryTarget: "var"
+        filename: minify ? 'flatline-browser.min.js' : 'flatline-browser.js',
+        library: 'Flatline',
+        libraryTarget: 'var'
     },
     resolve: {
-        extensions: [".ts", ".tsx"]
+        extensions: ['.ts', '.tsx']
     },
     module: {
         rules: [{
@@ -21,39 +22,39 @@ module.exports = [{
             loader: 'ts-loader'
         }]
     },
-    plugins: [/*
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false, dead_code: true },
-            mangle: true,
-            sourceMap: true,
-            beautify: false
-        })*/
-    ]
-}, {
-    entry: path.join(__dirname, 'src', 'Flatline.ts'),
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: "flatline-common.min.js",
-        libraryTarget: "commonjs"
-    },
-    resolve: {
-        extensions: [".ts", ".tsx"]
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            include: [
-                path.join(__dirname, 'src')
-            ],
-            loader: 'ts-loader'
-        }]
-    },
-    plugins: [
+    plugins: minify ? [
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false, dead_code: true },
             mangle: true,
             sourceMap: true,
             beautify: false
         })
-    ]
+    ] : []
+}, {
+    entry: path.join(__dirname, 'src', 'Flatline.ts'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: minify ? 'flatline-common.min.js' : 'flatline-common.js',
+        libraryTarget: 'commonjs'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            include: [
+                path.join(__dirname, 'src')
+            ],
+            loader: 'ts-loader'
+        }]
+    },
+    plugins: minify ? [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false, dead_code: true },
+            mangle: true,
+            sourceMap: true,
+            beautify: false
+        })
+    ] : []
 }]
