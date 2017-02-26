de60a8bfad0c5a1c8a3770c315c3ae1111ebda68
Christian Cygnus <gamer00@att.net>
2017-02-25 22:47:37 -0600

    Add style class

---
+++ src/Generation/Styles.ts
@@ -0,0 +1,274 @@
+export default class Styles {
+    //Color Properties
+    public color?: string;
+    public opacity?: string;
+
+    //Background and Border Properties
+    public background?: string;
+    public backgroundAttachment?: string;
+    public backgroundBlendMode?: string;
+    public backgroundColor?: string;
+    public backgroundImage?: string;
+    public backgroundPosition ?: string;
+    public backgroundRepeat ?: string;
+    public backgroundClip ?: string;
+    public backgroundOrigin ?: string;
+    public backgroundSize ?: string;
+    public border?: string;
+    public borderBottom?: string;
+    public borderBottomColor?: string;
+    public borderBottomLeftRadius?: string;
+    public borderBottomRightRadius?: string;
+    public borderBottomStyle?: string;
+    public borderBottomWidth?: string;
+    public borderColor?: string;
+    public borderImage?: string;
+    public borderImageOutset?: string;
+    public borderImageRepeat?: string;
+    public borderImageSlice?: string;
+    public borderImageSource?: string;
+    public borderImageWidth?: string;
+    public borderLeft?: string;
+    public borderLeftColor?: string;
+    public borderLeftStyle?: string;
+    public borderLeftWidth?: string;
+    public borderRadius?: string;
+    public borderRight?: string;
+    public borderRightColor?: string;
+    public borderRightStyle?: string;
+    public borderRightWidth?: string;
+    public borderStyle?: string;
+    public borderTop?: string;
+    public borderTopColor?: string;
+    public borderTopLeftRadius?: string;
+    public borderTopRightRadius?: string;
+    public borderTopStyle?: string;
+    public borderTopWidth?: string;
+    public borderWidth?: string;
+    public boxDecorationBreak?: string;
+    public boxShadow?: string;
+
+    //Basic Box Properties
+    public bottom?: string;
+    public clear?: string;
+    public clip?: string;
+    public display?: string;
+    public float?: string;
+    public height?: string;
+    public left?: string;
+    public margin?: string;
+    public marginBottom?: string;
+    public marginLeft?: string;
+    public marginRight?: string;
+    public marginTop?: string;
+    public maxHeight?: string;
+    public maxWidth?: string;
+    public minHeight?: string;
+    public minWidth?: string;
+    public overflow?: string;
+    public overflowX?: string;
+    public overflowY?: string;
+    public padding?: string;
+    public paddingBottom?: string;
+    public paddingLeft?: string;
+    public paddingRight?: string;
+    public paddingTop?: string;
+    public position?: string;
+    public right?: string;
+    public top?: string;
+    public visibility?: string;
+    public width?: string;
+    public verticalAlign?: string;
+    public zIndex?: string;
+
+    //Flexible Box Layout
+    public alignContent?: string;
+    public alignItems?: string;
+    public alignSelf?: string;
+    public flex?: string;
+    public flexBasis?: string;
+    public flexDirection?: string;
+    public flexFlow?: string;
+    public flexGrow?: string;
+    public flexShrink?: string;
+    public flexWrap?: string;
+    public justifyContent?: string;
+    public order?: string;
+
+    //Text Properties
+    public hangingPunctuation?: string;
+    public hyphens?: string;
+    public letterSpacing?: string;
+    public lineBreak?: string;
+    public lineHeight?: string;
+    public overflowWrap?: string;
+    public tabSize?: string;
+    public textAlign?: string;
+    public textAlignLast?: string;
+    public textCombineUpright?: string;
+    public textIndent?: string;
+    public textJustify?: string;
+    public textTransform?: string;
+    public whiteSpace?: string;
+    public wordBreak?: string;
+    public wordSpacing?: string;
+    public wordWrap?: string;
+
+    //Text textDecorationDecoration Properties
+    public textDecoration?: string;
+    public textDecorationColor?: string;
+    public textDecorationLine?: string;
+    public textDecorationStyle?: string;
+    public textShadow?: string;
+    public textUnderlinePosition?: string;
+    public fontFace?: string;
+    public fontFeatureValues?: string;
+    public font?: string;
+    public fontFamily?: string;
+    public fontFeatureSettings?: string;
+    public fontKerning?: string;
+    public fontLanguageOverride?: string;
+    public fontSize?: string;
+    public fontSizeAdjust?: string;
+    public fontStretch?: string;
+    public fontStyle?: string;
+    public fontSynthesis?: string;
+    public fontVariant?: string;
+    public fontVariantAlternates?: string;
+    public fontVariantCaps?: string;
+    public fontVariantEastAsian?: string;
+    public fontVariantLigatures?: string;
+    public fontVariantNumeric?: string;
+    public fontVariantPosition?: string;
+    public fontWeight?: string;
+
+    //Writing Modes Properties
+    public direction?: string;
+    public textOrientation?: string;
+    public unicodeBidi?: string;
+    public userSelect?: string;
+    public writingMode?: string;
+
+    //Table Properties
+    public borderCollapse?: string;
+    public borderSpacing?: string;
+    public captionSide?: string;
+    public emptyCells?: string;
+    public tableLayout?: string;
+
+    //Lists and Counters Properties
+    public counterIncrement?: string;
+    public counterReset?: string;
+    public listStyle?: string;
+    public listStyleImage?: string;
+    public listStylePosition?: string;
+    public listStyleType?: string;
+
+    //Animation Properties
+    public keyframes?: string;
+    public animation?: string;
+    public animationDelay?: string;
+    public animationDirection?: string;
+    public animationDuration?: string;
+    public animationFillMode?: string;
+    public animationIterationCount?: string;
+    public animationName?: string;
+    public animationPlayState?: string;
+    public animationTimingFunction?: string;
+    
+    //Transform Properties
+    public backfaceVisibility?: string;
+    public perspective?: string;
+    public perspectiveOrigin?: string;
+    public transform?: string;
+    public transformOrigin?: string;
+    public transformStyle?: string;
+
+    //Transition Properties
+    public transition?: string;
+    public transitionProperties?: string;
+    public transitionDuration?: string;
+    public transitionTimingFunction?: string;
+    public transitionDelay?: string;
+    
+    //Basic User Interface Properties
+    public boxSizing?: string;
+    public content?: string;
+    public cursor?: string;
+    public imeMode?: string;
+    public navDown?: string;
+    public navIndex?: string;
+    public navLeft?: string;
+    public navRight?: string;
+    public navUp?: string;
+    public outline?: string;
+    public outlineColor?: string;
+    public outlineOffset?: string;
+    public outlineStyle?: string;
+    public outlineWidth?: string;
+    public resize?: string;
+    public textOverflow?: string;
+
+    //Multi-column Layout Properties
+    public breakAfter?: string;
+    public breakBefore?: string;
+    public breakInside?: string;
+    public columnCount?: string;
+    public columnFill?: string;
+    public columnGap?: string;
+    public columnRule?: string;
+    public columnRuleColor?: string;
+    public columnRuleStyle?: string;
+    public columnRuleWidth?: string;
+    public columnSpan?: string;
+    public columnWidth?: string;
+    public columns?: string;
+    public widows?: string;
+
+    //Paged Media
+    public orphans?: string;
+    public pageBreakAfter?: string;
+    public pageBreakBefore?: string;
+    public pageBreakInside?: string;
+
+    //Generated Content for Paged Media
+    public marks?: string;
+    public quotes?: string;
+
+    //Filter Effects Propertoes
+    public filter?: string;
+
+    //Image Values and Replaced Content
+    public imageOrientation?: string;
+    public imageRendering?: string;
+    public imageResolution?: string;
+    public objectFit?: string;
+    public objectPosition?: string;
+
+    //Masking Properties
+    public mask?: string;
+    public maskType?: string;
+
+    //Speech Properties
+    public mark?: string;
+    public markAfter?: string;
+    public markBefore?: string;
+    public phonemes?: string;
+    public rest?: string;
+    public restAfter?: string;
+    public restBefore?: string;
+    public voiceBalance?: string;
+    public voiceDuration?: string;
+    public voicePitch?: string;
+    public voicePitchRange?: string;
+    public voiceRate?: string;
+    public voiceStress?: string;
+    public voiceVolume?: string;
+
+    //Marquee Properties
+    public marqueeDirection?: string;
+    public marqueePlayCount?: string;
+    public marqueeSpeed?: string;
+    public marqueeStyle?: string;
+    
+}
