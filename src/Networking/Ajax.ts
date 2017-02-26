aea00016d4e49097e4751f5a167b043f30b558e2
Alec Scratch <go.away.or.be.hacked@gmail.com>
2017-02-26 03:57:58 -0500

    Fixed Ajax

--- src/Networking/Ajax.ts
+++ src/Networking/Ajax.ts
@@ -1,52 +1,25 @@
-export function get(url: string, successCb: (response: any) => void, failureCb: (err: Error) => void): void {
-    /*const xhr = new XMLHttpRequest();
-    // Begin the XHR request
-    xhr.open('GET', url, true);
-
-    // Set up success handler
-    xhr.onload = () => {
-        const statusText = xhr.statusText;
-        // From Angular source:
-        // responseText is the old-school way of retrieving response (supported by IE9)
-        // response/responseType properties were introduced in XHR Level2 spec (supported by IE10)
-        const response = ('response' in xhr) ? xhr.response : xhr.responseText;
-        // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
-        const status = xhr.status === 1223 ? 204 : xhr.status;
-
-        try {
-            // Check status code and parse JSON if request succeeded
-            if (status >= 200 && status < 300) {
-                successCb(JSON.parse(response));   
-            }
-
-            // Return an error if status code is not in successful range
-            else {
-                failureCb(new Error(`Server responded with status code ${status}: ${statusText}`));
-            }
-        }
-
-        // Return an error if JSON parsing fails
-        catch (e) {
-            failureCb(new Error('Error parsing response JSON'));
-        }
-
-    };
-    
-    // Set up error handler
-    xhr.onerror = xhr.onabort = xhr.ontimeout = () => {
-        failureCb(new Error('Error processing AJAX request'));
-    };
-
-    // Send XHR request
-    xhr.send();*/
-    baseXhr(url, 'GET', (xhr) => xhr.send(), successCb, failureCb);
+export function get<T>(url: string, successCb?: (response: T) => void, failureCb?: (err: Error) => void): void {
+    nonebodyXhr(url, 'get', successCb, failureCb);
 }
 
-export function post(url: string, data: any, successCb: (response: any) => void, failureCb: (err: Error) => void): void {
-    baseXhr(url, 'post', (xhr) => {
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.send(JSON.stringify(data))
-    }, successCb, failureCb);
+export function head<T>(url: string, success?: (resp: T) => void, fail?: (res: Error) => void): void {
+    nonebodyXhr(url, 'head', success, fail);
+}
+
+export function options<T>(url: string, success?: (resp: T) => void, fail?: (res: Error) => void): void {
+    nonebodyXhr(url, 'options', success, fail);
+}
+
+export function del<T>(url: string, success?: (resp: T) => void, fail?: (res: Error) => void): void {
+    nonebodyXhr(url, 'delete', success, fail);
+}
+
+export function post<T>(url: string, data: any, successCb?: (response: T) => void, failureCb?: (err: Error) => void): void {
+    bodyXhr(url, 'post', data, successCb, failureCb);
+}
+
+export function put<T>(url: string, data: any, success?: (response: T) => void, fail?: (resp: Error) => void): void {
+    bodyXhr(url, 'put', data, success, fail);
 }
 
 export function baseXhr(url: string, method: string, onfinish: (res: XMLHttpRequest) => void, success?: (response: any) => void, fail?: (response: any) => void) {
@@ -90,76 +63,13 @@ export function baseXhr(url: string, method: string, onfinish: (res: XMLHttpRequ
     onfinish(xhr);
 }
 
-export function parameterize(obj: any){
-   function toQueryString(obj, urlEncode) {
-    //
-    // Helper function that flattens an object, retaining key structer as a path array:
-    //
-    // Input: { prop1: 'x', prop2: { y: 1, z: 2 } }
-    // Example output: [
-    //     { path: [ 'prop1' ],      val: 'x' },
-    //     { path: [ 'prop2', 'y' ], val: '1' },
-    //     { path: [ 'prop2', 'z' ], val: '2' }
-    // ]
-    //
-    function flattenObj(x, path?) {
-        var result = [];
-
-        path = path || [];
-        Object.keys(x).forEach(function (key) {
-            if (!x.hasOwnProperty(key)) return;
-
-            var newPath = path.slice();
-            newPath.push(key);
-
-            var vals = [];
-            if (typeof x[key] == 'object') {
-                vals = flattenObj(x[key], newPath);
-            } else {
-                vals.push({ path: newPath, val: x[key] });
-            }
-            vals.forEach(function (obj) {
-                return result.push(obj);
-            });
-        });
-
-        return result;
-    } // flattenObj
-
-    // start with  flattening `obj`
-    var parts = flattenObj(obj); // [ { path: [ ...parts ], val: ... }, ... ]
-
-    // convert to array notation:
-    parts = parts.map(function (varInfo) {
-        if (varInfo.path.length == 1) varInfo.path = varInfo.path[0];else {
-            var first = varInfo.path[0];
-            var rest = varInfo.path.slice(1);
-            varInfo.path = first + '[' + rest.join('][') + ']';
-        }
-        return varInfo;
-    }); // parts.map
-
-    // join the parts to a query-string url-component
-    var queryString = parts.map(function (varInfo) {
-        return varInfo.path + '=' + varInfo.val;
-    }).join('&');
-    if (urlEncode) return encodeURIComponent(queryString);else return queryString;
-}
-    return toQueryString(obj, true);
+export function nonebodyXhr<T>(url: string, method: string, success?: (resp: T) => void, fail?: (resp: Error) => void): void {
+    baseXhr(url, method, (xhr) => xhr.send(), success, fail);
 }
 
-
-var url = 'http://localhost:19196/Api/Test/Worked';
-
-var account = {
-    Email: 'test@test.com',
-    Password: 'test2@test.com'
+export function bodyXhr<T>(url: string, method: string, data: any, success?: (resp: T) => void, fail?: (resp: Error) => void){
+    baseXhr(url, method, (xhr) => {
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }, success, fail);
 }
-
-//console.log(parameterize(account));
-
-post(url, account, (res) => {
-    console.log(res);
-}, (res) => {
-    console.log(res);
-});
